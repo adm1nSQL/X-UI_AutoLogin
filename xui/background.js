071046797ffb3bda5e9113c646364a68ce55096f
Koi <58238284+adm1nSQL@users.noreply.github.com>
2023-01-09 18:07:17 +0800

    Update background.js

--- xui/background.js
+++ xui/background.js
@@ -4,14 +4,3 @@
 chrome.runtime.onInstalled.addListener(function () {
   console.log("插件已被安装");
 });
-
-chrome.contextMenus.create({
-	title: "测试右键菜单",
-	onclick: function(){
-		var a = document.getElementsByClassName("ant-input")[0];
-		alert(a.innerHTML)
-		// a.value="admin";
-		// document.getElementsByClassName("ant-input")[1].value="admin";
-		// alert(document.getElementsByClassName("ant-input")[0]);
-	}
-});
